381af4966c2064425213363b3ee44216824b0019
Chad Yu <cky25@cornell.edu>
2023-11-28 12:29:52 -0500

    chore: add language modeling head

--- HW 4/seagull/model/heads/seagull_lm.py
+++ HW 4/seagull/model/heads/seagull_lm.py
@@ -60,10 +60,37 @@ class SeagullLM(Module):
         use_kv_cache: bool = False,
         return_output_at_all_layers: bool = False,
         return_attentions: bool = False,
-    ) -> Tuple[torch.Tensor, Union[List[torch.Tensor], Tuple[List[torch.Tensor], List[torch.Tensor]]]]:
+    ) -> Tuple[
+        torch.Tensor,
+        Union[List[torch.Tensor], Tuple[List[torch.Tensor], List[torch.Tensor]]],
+    ]:
         """See: https://pages.github.coecis.cornell.edu/cs4740/hw4-fa23/seagull.model.heads.seagull_lm.html."""
         # TODO-6.2
-        raise NotImplementedError  # remove once the method is filled
+        # Obtain the output through the Seagull transformer.
+        seagull_output = self.seagull(
+            input_ids=input_ids,
+            position_ids=position_ids,
+            padding_mask=padding_mask,
+            use_kv_cache=use_kv_cache,
+            return_output_at_all_layers=return_output_at_all_layers,
+            return_attentions=return_attentions,
+        )
+
+        # Separate transformer layer outputs as necessary and compute logits.
+        all_outputs = seagull_output[0] if return_attentions else seagull_output
+        print(all_outputs[-1].size())
+        print(torch.t(self.seagull.embedding.token_embedding.weight.data).size())
+        logits = (
+            torch.matmul(
+                all_outputs[-1],
+                torch.t(self.seagull.embedding.token_embedding.weight.data),
+            )
+            if self.weight_tying
+            else self.lm_head(all_outputs[-1])
+        )
+
+        # Return the logits and the seagull output
+        return (logits, seagull_output)
 
     @torch.no_grad()
     def talk(
@@ -84,17 +111,29 @@ class SeagullLM(Module):
             # Dynamically populate the sequence length to "split" the tensor later.
             eos_token_batch_idxs = torch.where(input_ids_ == eos_token_id)[0].tolist()
             if len(eos_token_batch_idxs) > 0:
-                new_eos_token_batch_idxs = list(set(eos_token_batch_idxs) - modified_batch_idxs_)
-                all_sample_sequence_lengths_[new_eos_token_batch_idxs] = current_generation_length - 1
+                new_eos_token_batch_idxs = list(
+                    set(eos_token_batch_idxs) - modified_batch_idxs_
+                )
+                all_sample_sequence_lengths_[new_eos_token_batch_idxs] = (
+                    current_generation_length - 1
+                )
                 modified_batch_idxs_.update(new_eos_token_batch_idxs)
             return all_sample_sequence_lengths_, modified_batch_idxs_
 
         input_ids = input_ids.expand(num_samples, -1).to(self.device)
-        input_ids = input_ids[:, -self._max_positions :] if input_ids.shape[1] > self._max_positions else input_ids
-        position_ids = torch.arange(input_ids.shape[1]).expand(num_samples, -1).to(self.device)
+        input_ids = (
+            input_ids[:, -self._max_positions :]
+            if input_ids.shape[1] > self._max_positions
+            else input_ids
+        )
+        position_ids = (
+            torch.arange(input_ids.shape[1]).expand(num_samples, -1).to(self.device)
+        )
 
         all_input_ids = input_ids
-        all_sample_sequence_lengths = torch.ones(num_samples, dtype=torch.long) * (input_ids.shape[1] + max_new_tokens)
+        all_sample_sequence_lengths = torch.ones(num_samples, dtype=torch.long) * (
+            input_ids.shape[1] + max_new_tokens
+        )
         modified_batch_idxs = set()
 
         self.eval()  # https://ai.stackexchange.com/a/18392
@@ -102,13 +141,21 @@ class SeagullLM(Module):
             assert input_ids.shape == position_ids.shape
 
             # lm_logits: (num_samples, seq_length, vocab_size)
-            lm_logits = self(input_ids=input_ids.to(self.device), position_ids=position_ids, use_kv_cache=True)[0]
+            lm_logits = self(
+                input_ids=input_ids.to(self.device),
+                position_ids=position_ids,
+                use_kv_cache=True,
+            )[0]
             # lm_logits: (num_samples, vocab_size)
             lm_logits = lm_logits[:, -1, :] / temperature
 
             if top_k is not None:
-                top_k_vals, _ = torch.topk(lm_logits, k=top_k, dim=-1, largest=True, sorted=True)
-                lm_logits[lm_logits < top_k_vals[:, [-1]]] = -torch.finfo(lm_logits.dtype).max
+                top_k_vals, _ = torch.topk(
+                    lm_logits, k=top_k, dim=-1, largest=True, sorted=True
+                )
+                lm_logits[lm_logits < top_k_vals[:, [-1]]] = -torch.finfo(
+                    lm_logits.dtype
+                ).max
             probs = softmax(lm_logits, dim=-1, training=False)
             # input_ids: (num_samples, 1)
             input_ids = torch.multinomial(probs, num_samples=1)
@@ -120,7 +167,10 @@ class SeagullLM(Module):
                 else position_ids[:, -1].unsqueeze(1)
             )
             if eos_token_id is not None:
-                all_sample_sequence_lengths, modified_batch_idxs = _get_updated_sequence_lengths(
+                (
+                    all_sample_sequence_lengths,
+                    modified_batch_idxs,
+                ) = _get_updated_sequence_lengths(
                     input_ids_=input_ids,
                     current_generation_length=all_input_ids.shape[1],
                     all_sample_sequence_lengths_=all_sample_sequence_lengths,
@@ -130,5 +180,8 @@ class SeagullLM(Module):
         self.reset_kv_cache()  # reset start position to zero
         all_input_ids = all_input_ids.tolist()
         if eos_token_id is not None:
-            all_input_ids = [seq[: all_sample_sequence_lengths[seq_idx]] for seq_idx, seq in enumerate(all_input_ids)]
+            all_input_ids = [
+                seq[: all_sample_sequence_lengths[seq_idx]]
+                for seq_idx, seq in enumerate(all_input_ids)
+            ]
         return all_input_ids
